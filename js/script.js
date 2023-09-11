5675e8fefe8d6d0b951b0618c79ac12d5f4dd560
sm-mahi-abdullah <142378517+sm-mahi-abdullah@users.noreply.github.com>
2023-09-11 15:19:16 +0600

    test

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector(".hamburger");
+let slidingMenu = document.querySelector(".sliding-mobile-menu");
+hamburger.addEventListener("click",()=>{
+    slidingMenu.classList.toggle("slide");
+})
+
